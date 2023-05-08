d8a577b967ce1a4021cc7ea2e33d7b017690b44a
Gemma Fong <115029730+smolTypo@users.noreply.github.com>
2023-05-07 20:20:49 -0400

    Update http_server.js

--- http_server.js
+++ http_server.js
@@ -5,6 +5,7 @@ const fs = require('lowdb/adapters/FileSync');
 const adapter = new fs('db.json');
 const db = low(adapter);
 const cors = require('cors');
+const PORT = process.env.PORT || 3000;
 // const { faker } = require('@faker-js/faker');
 
 // allow cross-origin resource sharing (CORS)
